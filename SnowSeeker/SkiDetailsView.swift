228c9b43be7af7679425e3bafdb3140e6d49da43
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-08-17 11:52:02 +0100

    Creating a secondary view for NavigationView

---
+++ SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,27 @@
+//
+//  SkiDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Gary Watson on 17/08/2020.
+//  Copyright © 2020 Gary Watson. All rights reserved.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    let resort: Resort
+    
+    var body: some View {
+        VStack {
+            Text("Elevation: \(resort.elevation)m")
+            Text("Snow: \(resort.snowDepth)cm")
+        }
+        
+    }
+}
+
+struct SkiDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SkiDetailsView(resort: Resort.example)
+    }
+}
